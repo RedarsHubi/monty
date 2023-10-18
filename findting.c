89e8aef8bbb666f9c9dd77ce62c5e2ebd4adbc1d
RedarsHubi <RedarsHubi@github.com>
2023-10-18 10:25:45 -0700

    0

--- findting.c
+++ findting.c
@@ -19,21 +19,6 @@ void get_op(char *op, stack_t **stack, unsigned int line_number)
 	instruction_t valid_ops[] = {
 		{"push", m_push},
 		{"pall", m_pall},
-		{"pint", m_pint},
-		{"pop", m_pop},
-		{"swap", m_swap},
-		{"add", m_add},
-		{"nop", m_nop},
-		{"sub", m_sub},
-		{"mul", m_mul},
-		{"div", m_div},
-		{"mod", m_mod},
-		{"rotl", rotl},
-		{"rotr", rotr},
-		{"stack", m_stack},
-		{"queue", m_queue},
-		{"pchar", m_pchar},
-		{"pstr", m_pstr},
 		{NULL, NULL}
 	};
 
